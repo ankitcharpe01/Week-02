2a5f525cf9e87abe3b002c65feef800480cc79c2
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-27 10:16:41 +0530

    Online Food Delivery System Added Successfully...

---
+++ onlinefooddeliverysystem/Discountable.java
@@ -0,0 +1,6 @@
+package com.encapsulationandpolymorphism.onlinefooddeliverysystem;
+
+public interface Discountable {
+    public void applyDiscount(double discount);
+    public double getDiscountDetails();
+}
