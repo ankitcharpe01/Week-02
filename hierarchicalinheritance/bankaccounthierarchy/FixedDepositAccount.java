324c622a9f601b5a7d5f222783bc18dc75403da5
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-27 10:05:54 +0530

    Hierarchical Inheritance Added...

---
+++ hierarchicalinheritance/bankaccounthierarchy/FixedDepositAccount.java
@@ -0,0 +1,21 @@
+package com.inheritance.hierarchicalinheritance.bankaccounthierarchy;
+
+class FixedDepositAccount extends BankAccount {
+    private int depositYear;
+
+    public FixedDepositAccount(long accountNumber, double balance, int depositYear) {
+        super(accountNumber, balance);
+        this.depositYear = depositYear;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account type is fixed deposit account!");
+    }
+
+    @Override
+    public void display() {
+        displayAccountType();
+        super.display();
+        System.out.println("Amount fixed for " + depositYear + " years");
+    }
+}
